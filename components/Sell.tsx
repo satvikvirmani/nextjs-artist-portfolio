9d301042babc77610c1542940bc085f6af68d278
Satvik Virmani <70750314+SatvikVirmani@users.noreply.github.com>
2021-07-10 12:31:46 +0530

    Fixed typo😉

--- components/Sell.tsx
+++ components/Sell.tsx
@@ -4,7 +4,7 @@ export const Sell: React.SFC = () => {
   return (
     <>
       <div className="py-6 col-span-10 col-start-2 col-end-12 text-center font-light text-gray-600 text-base">
-        This website available is for sale.{" "}
+        This website is available for sale.{" "}
         <a
           href="https://www.instagram.com/satvikvirmani/"
           className="underline mx-2"

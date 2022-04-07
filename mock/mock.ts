04799ba60a58bb909f856ae3db05b613decfa75a
Satvik Virmani <70750314+satvikvirmani@users.noreply.github.com>
2022-04-07 22:37:19 +0530

    Update mock.ts

--- mock/mock.ts
+++ mock/mock.ts
@@ -1,11 +1,11 @@
 export const data = {
-  name: "Carina Symth",
+  name: "Garima Malik",
   job: "Portrait Artist",
-  email: "carinasymth@icloud.com",
-  instagram_link: "https://www.instagram.com/satvikvirmani/",
-  facebook_link: "https://www.facebook.com/satvik.virmani.9",
+  email: "garimamalik28199@gmail.com",
+  instagram_link: "https://www.instagram.com/color_world99/",
+  facebook_link: "https://www.facebook.com/garima.malik.1004",
   aboutme_para1:
-    "​Carina Symth began their professional artistic journey when they appeared on the New York art scene in 2000. Since then, they have participated in a number of collaborative projects and have exhibited their work throughout the world",
+    "​Garima Malik began their professional artistic journey in Fine Arts from the Delhi Collage of Arts where I earned my qualification in Super Professional Diploma. Being a literature student helps a lot in defining art. Art is similar to poetry. Painting and poetry are similar in the way that they make “absent things present",
   aboutme_para2:
-    "Unlike many of their contemporaries, Carina Symth is primarily self-taught and has their own approach to - and definition of - art. They believe in constantly taking risks and believe that inspiration can be found wherever they look. To find out more about their philosophy, feel free to get in touch.",
+    "Unlike many of their contemporaries, Garima Malik is primarily self-taught and has their own approach to - and definition of - art. They believe in constantly taking risks and believe that inspiration can be found wherever they look. To find out more about their philosophy, feel free to get in touch.",
 };

ec795effb57db9586eddd83de0a525c535d5f508
Satvik Virmani <70750314+satvikvirmani@users.noreply.github.com>
2022-04-07 22:45:13 +0530

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -48,7 +48,7 @@ export const Hero: React.SFC = () => {
         <div className="relative grid place-items-center order-1 md:order-2">
           <Image
             loader={modelImageLoader}
-            src="https://res.cloudinary.com/wecloud/image/upload/v1625592156/artist-portfolio/model_axxy6f.jpg"
+            src="https://res.cloudinary.com/wecloud/image/upload/v1649351673/artist-portfolio/model_uhpi8y.jpg"
             alt="Picture of artist"
             objectFit="contain"
             height={imageDimensions[1]}

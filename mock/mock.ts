12beec7d2f5345992358bb8cd133c5fd80935ff5
Satvik Virmani <70750314+satvikvirmani@users.noreply.github.com>
2022-09-11 13:04:13 +0530

    update mock.ts

--- mock/mock.ts
+++ mock/mock.ts
@@ -1,11 +1,11 @@
 export const data = {
-  name: "Garima Malik",
+  name: "Carima Smith",
   job: "Portrait Artist",
-  email: "garimamalik28199@gmail.com",
-  instagram_link: "https://www.instagram.com/color_world99/",
-  facebook_link: "https://www.facebook.com/garima.malik.1004",
+  email: "virmanisatvik01@gmail.com",
+  instagram_link: "https://www.instagram.com/satvikvirmani/",
+  facebook_link: "https://www.facebook.com/satvik.virmani.9",
   aboutme_para1:
-    "​Garima Malik began their professional artistic journey in Fine Arts from the Delhi Collage of Arts where I earned my qualification in Super Professional Diploma. Being a literature student helps a lot in defining art. Art is similar to poetry. Painting and poetry are similar in the way that they make “absent things present",
+    "Carima Smith began their professional artistic journey in Fine Arts from the Columbia Collage of Arts where I earned my qualification in Super Professional Diploma. Being a literature student helps a lot in defining art. Art is similar to poetry. Painting and poetry are similar in the way that they make “absent things present",
   aboutme_para2:
-    "Unlike many of their contemporaries, Garima Malik is primarily self-taught and has their own approach to - and definition of - art. They believe in constantly taking risks and believe that inspiration can be found wherever they look. To find out more about their philosophy, feel free to get in touch.",
+    "Unlike many of their contemporaries, Carina Smith is primarily self-taught and has their own approach to - and definition of - art. They believe in constantly taking risks and believe that inspiration can be found wherever they look. To find out more about their philosophy, feel free to get in touch.",
 };
